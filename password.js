87ab4dc6aa9b9b99fce562eef6aec2a0bb33a482
Mahsa Maleki <mahsamaleki088@gmail.com>
2020-05-28 16:31:18 -0700

    add username validity

--- password.js
+++ password.js
@@ -8,9 +8,14 @@
         passwordBtn.innerHTML = checkValidity(document.getElementById("password").value,document.getElementById("username").value);
     })
       function checkValidity(pass,username){
-     if (pass =="123456")
+     if (pass =="123456" && username=='Mahsa')
      window.open("http://www.google.ca");
-     else
-     window.alert("dear "+ username +"the password in not correct");
-      }
+     else {
+     if (pass =="123456" && username !='Mahsa')
+     window.alert("dear "+ username +"the Username is not correct");
+     if (pass !="123456" && username =='Mahsa')
+     window.alert("dear "+ username +"the password is not correct");
+     if (pass !="123456" && username !='Mahsa')
+     window.alert("dear "+ username +"the password and username are not correct");
+      }}
   })();
