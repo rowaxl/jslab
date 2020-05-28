f1a8240fd58f138730317ffb3acd3acdb5a6c25b
Mahsa Maleki <mahsamaleki088@gmail.com>
2020-05-28 16:21:55 -0700

    add username

--- password.js
+++ password.js
@@ -1,15 +1,16 @@
 (function() {
+    const username=document.getElementById("username");
     const password=document.getElementById("password");
     const passwordBtn=document.getElementById("passwordBtn");
-
+    
   
     passwordBtn.addEventListener('click', function(e) {
-        passwordBtn.innerHTML = checkValidity(document.getElementById("password").value);
+        passwordBtn.innerHTML = checkValidity(document.getElementById("password").value,document.getElementById("username").value);
     })
-      function checkValidity(pass){
+      function checkValidity(pass,username){
      if (pass =="123456")
      window.open("http://www.google.ca");
      else
-     window.alert("the password in not correct");
+     window.alert("dear "+ username +"the password in not correct");
       }
   })();
