ece3d7035cdce6af7c724718a6fc38c8c1f6f548
rowaxl <rowaxl0@gmail.com>
2020-05-28 16:09:47 -0700

    minor change

--- js.js
+++ js.js
@@ -1,14 +1,17 @@
-const userName = "wonjae.kim@gmail.com";
-const password = "Passw0rd!"
-
-let validate = () => {
-  const idValue = document.getElementById('user-name').value
-  const passwordValue = document.getElementById('password').value
-
-  if (userName === idValue && password === passwordValue) {
-    document.getElementById('form').style.display = 'none';
-    document.getElementById('result').style.display = 'block';
-  } else {
-    alert("Invalid user name or password!")
+(function () {
+  const userName = 'wonjae.kim@gmail.com';
+  const password = 'Passw0rd!';
+  let validate = () => {
+    const idValue = document.getElementById('user-name').value
+    const passwordValue = document.getElementById('password').value
+  
+    if (userName === idValue && password === passwordValue) {
+      document.getElementById('form').style.display = 'none';
+      document.getElementById('result').style.display = 'block';
+    } else {
+      alert("Invalid user name or password!")
+    }
   }
-}
+
+  document.getElementById('btn-signin').addEventListener('click', validate)
+})();
